2fcdd2b0b945f7be38d48d3567ce333c1ab23076
spybitstrix <djspyder479@gmail.com>
2026-04-22 03:00:40 +0530

    Add comments and inline descriptions
    Updated script purpose to clarify quality checks performed.

--- tests/constraints_checks_gold.sql
+++ tests/constraints_checks_gold.sql
@@ -3,16 +3,19 @@
 Quality Checks
 ===============================================================================
 Script Purpose:
-    This script performs quality checks to validate the integrity, consistency, 
-    and accuracy of the Gold Layer. These checks ensure:
+    This script performs various quality checks for data consistency, accuracy, 
+    and standardization across the 'gold' layer. It includes checks for:
     - Uniqueness of surrogate keys in dimension tables.
     - Referential integrity between fact and dimension tables.
     - Validation of relationships in the data model for analytical purposes.
-
+    
 Usage Notes:
+    - Run these checks after data loading Silver Layer.
     - Investigate and resolve any discrepancies found during the checks.
 ===============================================================================
 */
+
+
 
 -- ====================================================================
 -- Checking 'gold.dim_customers'
